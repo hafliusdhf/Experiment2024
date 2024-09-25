e901887341366ec5e68399feb02c7bad72ffdc15
Administrator <70838697@qq.com>
2024-09-25 09:37:29 +0800

    第4次课演示了控件的事件响应，包括匿名类，内部类和Lamda表达式，Toast以及AlertDialog的使用

--- app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
+++ app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.casper.Experiment2024;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String clickedText = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +26,64 @@ public class MainActivity extends AppCompatActivity {
         //使用布局文件创建控件
         setContentView(R.layout.activity_main);
         TextView textViewHelloWorld = findViewById(R.id.textview_hello_world);
-        textViewHelloWorld.setText(R.string.hello_china);
-        textViewHelloWorld.setText(R.string.hello_jnu);
+        textViewHelloWorld.setOnClickListener(new TextViewOnClickListener());
+        TextView textViewHelloChina = findViewById(R.id.textview_hello_china);
+        textViewHelloChina.setOnClickListener(new TextViewOnClickListener());
+        TextView textViewHelloJNU = findViewById(R.id.textview_hello_jnu);
+        textViewHelloJNU.setOnClickListener(new TextViewOnClickListener());
 
-        /*
-        RelativeLayout relativeLayout = new RelativeLayout(this);
+        TextView textViewHelloWorld1 = findViewById(R.id.textview_hello_world1);
+        textViewHelloWorld1.setOnClickListener(new TextViewOnClickListener());
+        TextView textViewHelloChina1 = findViewById(R.id.textview_hello_china1);
+        textViewHelloChina1.setOnClickListener(new TextViewOnClickListener());
+        TextView textViewHelloJNU1 = findViewById(R.id.textview_hello_jnu1);
+        textViewHelloJNU1.setOnClickListener(new TextViewOnClickListener());
 
-        RelativeLayout.LayoutParams params =  new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.CENTER_IN_PARENT);  //设置布局中的控件居中显示
-        TextView textView = new TextView(this);                       //创建TextView控件
-        textView.setText("Java 代码实现界面布局");                     //设置TextView的文字内容
-        textView.setTextColor(Color.RED);                                  //设置TextView的文字颜色
-        textView.setTextSize(18);                                                //设置TextView的文字大小
-        relativeLayout.addView(textView, params);                  //添加TextView对象和TextView的布局属性
+        Button buttonShowMessage = findViewById(R.id.button_show_message);
+        buttonShowMessage.setOnClickListener(buttonView -> {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle(R.string.message);
+                    builder.setMessage(getString(R.string.you_have_clicked) + clickedText);
+                    builder.setIcon(R.mipmap.ic_launcher_round);
+                    //点击对话框以外的区域是否让对话框消失
+                    builder.setCancelable(true);
+                    //设置正面按钮
+                    builder.setPositiveButton(R.string.positive, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, getString(R.string.you_clicked_yes), Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    });
 
-        setContentView(relativeLayout);                                  //设置在Activity中显示RelativeLayout
-        */
+                    //设置反面按钮
+                    builder.setNegativeButton("Negative", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, R.string.you_clicked_no, Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    });
+
+                    AlertDialog dialog = builder.create();
+                    //对话框显示的监听事件
+                    dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                        @Override
+                        public void onShow(DialogInterface dialog) {
+                            Log.e("MainActivity", getString(R.string.the_dialog_shows));
+                        }
+                    });
+                    //对话框消失的监听事件
+                    dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            Log.e("MainActivity", getString(R.string.the_dialog_disppeared));
+                        }
+                    });
+                    //显示对话框
+                    dialog.show();
+                }
+        );
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -43,5 +91,16 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+    }
+
+
+    private class TextViewOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View textViewClicked) {
+            TextView textView = (TextView) textViewClicked;
+            clickedText = (String) ((TextView) textViewClicked).getText();
+            Toast.makeText(MainActivity.this, clickedText, Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
