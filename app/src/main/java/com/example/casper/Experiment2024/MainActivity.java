e5d7d97386ac2658d535357e3217e327f73e9023
Administrator <70838697@qq.com>
2024-11-06 09:28:17 +0800

    第九次课用Tablayout建立滑动标签，演示如何把Activity代码移动到Fragment，注意Fragment的生命周期

--- app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
+++ app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
@@ -21,190 +21,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.casper.Experiment2024.data.DataBank;
 import com.example.casper.Experiment2024.data.Item;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<Item> items;
-    private ActivityResultLauncher<Intent> launcherAdd;
-    private ActivityResultLauncher<Intent> launcherUpdate;
-    private ShopItemAdapter shopItemAdapter;
+    private String[] tabHeaderStrings={"Items","News","Map"};
 
-    @Override
-    public boolean onContextItemSelected(MenuItem itemMenu ) {
-        //item.getOrder()
-        switch (itemMenu.getItemId()) {
-            case 0:
-                Intent intentAdd = new Intent(MainActivity.this, ItemActivity.class);
-                intentAdd.putExtra("position", itemMenu.getOrder());
-                launcherAdd.launch(intentAdd);
-                break;
-            case 1:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle(R.string.confirmation)
-                        .setMessage(R.string.are_you_sure_you_want_to_delete_the_item)
-                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                items.remove(itemMenu.getOrder());
-                                shopItemAdapter.notifyItemRemoved(itemMenu.getOrder());
-                                DataBank databank=new DataBank(MainActivity.this);
-                                databank.saveItems(items);
-                            }
-                        })
-                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                // 用户取消删除
-                                dialog.dismiss();
-                            }
-                        });
-                // 创建并显示对话框
-                AlertDialog dialog = builder.create();
-                dialog.show();
-                break;
-            case 2:
-                Intent intentUpdate = new Intent(MainActivity.this, ItemActivity.class);
-                intentUpdate.putExtra("position", itemMenu.getOrder());
-                Item itemObject= items.get(itemMenu.getOrder());
-                intentUpdate.putExtra("item_name", itemObject.getTitle());
-                intentUpdate.putExtra("item_price", itemObject.getPrice());
-                launcherUpdate.launch(intentUpdate);
-                break;
-            default:
-                return super.onContextItemSelected(itemMenu);
-        }
-        return true;
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        launcherAdd = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                        assert data != null;
-                        String itemName=data.getStringExtra("item_name");
-                        double itemPrice= data.getDoubleExtra("item_price",0);
-                        int position = data.getIntExtra("position",0);
-                        items.add(position,new Item(itemName, itemPrice, R.drawable.qingjiao));
-                        shopItemAdapter.notifyItemInserted(position);
-                        DataBank databank=new DataBank(MainActivity.this);
-                        databank.saveItems(items);
-                    }
-                });
-        launcherUpdate = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                        assert data != null;
-                        String itemName = data.getStringExtra("item_name");
-                        double itemPrice = data.getDoubleExtra("item_price", 0);
-                        int position = data.getIntExtra("position", 0);
-                        Item item = items.get(position);
-                        item.setTitle(itemName);
-                        item.setPrice(itemPrice);
-                        shopItemAdapter.notifyItemChanged(position);
-                        DataBank databank=new DataBank(MainActivity.this);
-                        databank.saveItems(items);
-                    }
-                });
-
-        EdgeToEdge.enable(this);
-        //使用布局文件创建控件
         setContentView(R.layout.activity_main);
-
-        RecyclerView mainRecyclerView = findViewById(R.id.recyclerview_items);
-        mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        DataBank databank=new DataBank(this);
-        items= databank.readItems();
-        if(items.isEmpty())
-            items.add(new Item("baicai", 1.5, R.drawable.baicai));
+        // 获取ViewPager2和TabLayout的实例
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        // 创建适配器
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager.setAdapter(fragmentAdapter);
 
 
-        shopItemAdapter = new ShopItemAdapter(items);
-        mainRecyclerView.setAdapter(shopItemAdapter);
-
-        registerForContextMenu(mainRecyclerView);
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
+        // 将TabLayout和ViewPager2进行关联
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(tabHeaderStrings[position])
+        ).attach();
     }
-
-    private class ShopItemAdapter extends RecyclerView.Adapter {
-        private final List<Item> items;
-
-        public ShopItemAdapter(List<Item> items) {
-            this.items=items;
+    public class FragmentAdapter extends FragmentStateAdapter {
+        private static final int NUM_TABS = 3;
+        public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
         }
 
         @NonNull
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item, parent, false);
-            return new MyViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-            MyViewHolder myViewHolder= (MyViewHolder) holder;
-            myViewHolder.getTextViewName().setText(items.get(position).getTitle());
-            myViewHolder.getTextViewPrice().setText("" + items.get(position).getPrice());
-            myViewHolder.getImageViewPicture().setImageResource(items.get(position).getResourceId());
+        public Fragment createFragment(int position) {
+            // 根据位置返回对应的Fragment实例
+            switch (position) {
+                case 0:
+                    return ShoppingListFragment.newInstance();
+                case 1:
+                    return WebViewFragment.newInstance("http://news.sina.com.cn");
+                case 2:
+                    return new ShoppingListFragment();
+                    //return new WebViewFragment();
+                default:
+                    return null;
+            }
         }
 
         @Override
         public int getItemCount() {
-            return items.size();
-        }
-
-        private class MyViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
-            public ImageView getImageViewPicture() {
-                return imageViewPicture;
-            }
-
-            public TextView getTextViewName() {
-                return textViewName;
-            }
-
-            public TextView getTextViewPrice() {
-                return textViewPrice;
-            }
-
-            private final ImageView imageViewPicture;
-            private final TextView textViewName;
-            private final TextView textViewPrice;
-
-            public MyViewHolder(@NonNull View itemView) {
-                super(itemView);
-                this.imageViewPicture= itemView.findViewById(R.id.imageview_item);
-                this.textViewName= itemView.findViewById(R.id.textview_item_name);
-                this.textViewPrice=itemView.findViewById(R.id.textview_item_price);
-
-                itemView.setOnCreateContextMenuListener(this);
-            }
-
-            @Override
-            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-                menu.setHeaderTitle("具体操作");
-
-                menu.add(0, 0, this.getAdapterPosition(), "添加" + this.getAdapterPosition());
-                menu.add(0, 1, this.getAdapterPosition(), "删除" + this.getAdapterPosition());
-                menu.add(0, 2, this.getAdapterPosition(), "修改" + this.getAdapterPosition());
-            }
+            return NUM_TABS;
         }
     }
 }
