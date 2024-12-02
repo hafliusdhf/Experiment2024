98d886c052191727b4eb87b81368ae6c25ff24d7
林钊民 <2649184910@qq.com>
2024-12-02 16:39:07 +0800

    陈老师您好！这是“实验3‘修改TextView’的内容”，恳请检查！

--- app/src/main/java/com/example/casper/Experiment2024/Hello1880Activity.java
+++ app/src/main/java/com/example/casper/Experiment2024/Hello1880Activity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
 
 public class Hello1880Activity extends AppCompatActivity {
 
@@ -15,6 +16,8 @@ public class Hello1880Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_hello1880);
+        TextView textView = findViewById(R.id.textview_helloworld);
+        textView.setText(R.string.hello_jnu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
