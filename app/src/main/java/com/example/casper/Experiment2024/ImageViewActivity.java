087570a4af5657780c1821289ff7411491e5b723
林钊民 <2649184910@qq.com>
2024-12-03 19:45:03 +0800

    陈老师您好！这是“实验5”，恳请检查！

---
+++ app/src/main/java/com/example/casper/Experiment2024/ImageViewActivity.java
@@ -0,0 +1,55 @@
+package com.example.casper.Experiment2024;//package com.example.casper.Experiment2024;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ImageViewActivity extends AppCompatActivity {
+    private Button buttonPrevious = (Button)findViewById(R.id.button_previous), buttonNext =(Button) findViewById(R.id.buttonview_input);
+    private ImageView imageViewFunny = (ImageView)findViewById(R.id.image_view_funny);
+    private int[] imageIDArray = {R.drawable.funny_1, R.drawable.funny_2
+            , R.drawable.funny_3, R.drawable.funny_4, R.drawable.funny_5
+            , R.drawable.funny_6
+    };
+    private int imageIDArrayCurrentIndex;
+
+    public ImageViewActivity(int iTest) {
+        imageIDArrayCurrentIndex = 0;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_image_view);
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });*/
+
+        buttonPrevious.setOnClickListener(new MyButtonClickListener());
+        buttonNext.setOnClickListener(new MyButtonClickListener());
+
+    }
+    private class MyButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (((Button) view).getText() == "下一个") {
+                imageIDArrayCurrentIndex ++;
+            } else {
+                imageIDArrayCurrentIndex --;
+            }
+            imageViewFunny.setImageResource(imageIDArray[imageIDArrayCurrentIndex]);
+        }
+    }
+
+}
