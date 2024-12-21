5a68ed0bb03ee2a130682bf250e85c67eb0d24f7
林钊民 <2649184910@qq.com>
2024-12-21 12:18:39 +0800

    陈老师您好！这是“实验10”，恳请检查！

---
+++ app/src/main/java/com/example/casper/Experiment2024/fragment/MarkerOptions.java
@@ -0,0 +1,9 @@
+package com.example.casper.Experiment2024.fragment;
+
+import com.tencent.tencentmap.mapsdk.maps.model.LatLng;
+
+public class MarkerOptions {
+    public com.tencent.tencentmap.mapsdk.maps.model.MarkerOptions position(LatLng jnuLocation) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/casper/Experiment2024/fragment/WebViewFragment.java
+++ app/src/main/java/com/example/casper/Experiment2024/fragment/WebViewFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.casper.Experiment2024.R;
 
 public class WebViewFragment extends Fragment {
+    private WebView webView;
 
     public WebViewFragment() {
         super(R.layout.fragment_web_view); // 这里需要创建相应的布局文件
@@ -18,6 +22,12 @@ public class WebViewFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+        webView = view.findViewById(R.id.web_view); // 确保在布局文件中有这个ID
+        webView.setWebViewClient(new WebViewClient()); // 使得点击链接时在WebView内打开
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // 启用JavaScript
+        webView.loadUrl("http://www.baidu.com"); // 加载百度首页
+        return view;
     }
 }
